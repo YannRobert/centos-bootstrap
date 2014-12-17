b83f5a75fcc3d5bd3a743d4e8179ddd9af8639f0
Yann ROBERT <yann.robert@anantaplex.fr>
2014-12-17 13:31:26 +0100

    if EPEL is already installed, should not exit the script

--- setup.sh
+++ setup.sh
@@ -7,7 +7,12 @@ set -e
 
 yum update -y
 
+# install EPEL, if already installed, should not exit the script
+set +e
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+
+set -e
+
 yum update -y
 
 yum install -y fail2ban
