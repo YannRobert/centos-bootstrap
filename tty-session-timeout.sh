2c46a6ae2d79d455683fd1b7bda0aa69b667c61b
Yann ROBERT <yann.robert@anantaplex.fr>
2015-01-07 19:33:38 +0100

    simpler grep to work + indent

--- tty-session-timeout.sh
+++ tty-session-timeout.sh
@@ -1,10 +1,10 @@
 # set a timeout policy for bash shell running on the console
 # this file should be copied to the /etc/profile.d/ directory
-if [ $(tty | grep "^/etc/tty") ];
+if [ $(tty | grep "tty") ];
 then 
-	TMOUT=300;
-	readonly TMOUT;
-	export TMOUT;
-	echo "Session will timeout in $TMOUT seconds"
+   TMOUT=300;
+   readonly TMOUT;
+   export TMOUT;
+   echo "Session will timeout in $($TMOUT/60) minutes"
 fi
 
