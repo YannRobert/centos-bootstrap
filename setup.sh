769729deb925f8df49bdde8c2645655952dc33a6
Yann ROBERT <yann.robert@anantaplex.fr>
2015-01-15 19:02:56 +0000

    support that docker fails to start

--- setup.sh
+++ setup.sh
@@ -47,7 +47,7 @@ yum install -y net-tools nc
 yum install -y docker-io
 # start the docker service on boot
 chkconfig docker on 
-sudo systemctl start docker.service
+{ systemctl start docker.service } || { echo "docker.service failed to start (it may be normal on some VPS managed by OpenVZ)" }
 
 # we want nslookup 
 yum install -y bind-utils
