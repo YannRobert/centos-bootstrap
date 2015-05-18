23fc7bbe3274dc3ae6e44c5b8fff11cbdf53354f
Yann ROBERT <yann.robert@anantaplex.fr>
2015-05-18 13:53:46 +0000

    upgrade docker-compose to version 1.2.0

--- setup.sh
+++ setup.sh
@@ -53,9 +53,9 @@ chkconfig docker on
 }
 
 # install docker-compose (previously known as fig)
-curl -L https://github.com/docker/compose/releases/download/1.1.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-curl -L https://raw.githubusercontent.com/docker/compose/1.1.0/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
+curl -L https://raw.githubusercontent.com/docker/compose/1.2.0/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
 
 # we want nslookup 
 yum install -y bind-utils
