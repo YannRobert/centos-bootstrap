1c3ce0831dac49b2fc201785eccee4c50a5683f2
Yann ROBERT <yann.robert@anantaplex.fr>
2015-11-10 13:23:07 +0100

    timezone should not be affected by the setup.sh script. Should it be necessary, a separate script set-timezone-to-UTC.sh can be called

---
+++ set-timezone-to-UTC.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# set the system to use UTC timezone
+ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+
+
--- setup.sh
+++ setup.sh
@@ -9,9 +9,6 @@
 set -e
 set -x
 
-# set the system to use UTC timezone
-ln -sf /usr/share/zoneinfo/UTC /etc/localtime
-
 # if dnf is available, then use dnf, otherwise use yum, as the package manager command
 PM=yum
 if test -f /usr/bin/dnf
