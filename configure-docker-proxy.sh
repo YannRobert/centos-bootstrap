491693eac83414391d4962d12e6422e31f678416
Yann ROBERT <yann.robert@anantaplex.fr>
2016-01-13 18:04:21 +0100

    using recommanded way to set the http proxy into docker configuration, as documented on https://docs.docker.com/engine/articles/systemd/

--- configure-docker-proxy.sh
+++ configure-docker-proxy.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+#
+# this script automate configuration of the HTTP_PROXY into docker
+#
+# see documentation for docker with systemd
+# https://docs.docker.com/engine/articles/systemd/
+#
+
 # this script must be run as privileged user
 if [ $(id -u) -ne 0 ] ; then
    echo "Error : this script requires sudo"
@@ -19,14 +26,23 @@ PROXY_PORT=$2
 
 echo "Will use http://${PROXY_HOST}:${PROXY_PORT} as the proxy to use with docker"
 
-sed -i '/^HTTP_PROXY=/d' /etc/sysconfig/docker
-sed -i '/^HTTPS_PROXY=/d' /etc/sysconfig/docker
+# create a systemd drop-in directory for the docker service
+mkdir -p /etc/systemd/system/docker.service.d/
 
-echo "export HTTP_PROXY=\"http://${PROXY_HOST}:${PROXY_PORT}\"" >> /etc/sysconfig/docker
-echo "export HTTPS_PROXY=\$HTTP_PROXY" >> /etc/sysconfig/docker
+# Now create a file called /etc/systemd/system/docker.service.d/http-proxy.conf that adds the HTTP_PROXY environment variable
+cat << EOF > /etc/systemd/system/docker.service.d/http-proxy.conf
+[Service]
+Environment="HTTP_PROXY=http://${PROXY_HOST}:${PROXY_PORT}/" "NO_PROXY=localhost,127.0.0.1"
+EOF
 
-# restart the docker service
-service docker restart
+# Flush changes
+sudo systemctl daemon-reload
+
+# Verify that the configuration has been loaded
+sudo systemctl show docker --property Environment
+
+# Restart Docker
+systemctl restart docker
 
 # pulling the busybox image in order to test everything is fine
 docker pull busybox
