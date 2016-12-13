93a0121e23c1c9797ef6b7fb0bbab602e57c3424
Yann ROBERT <yann.robert@anantaplex.fr>
2016-12-13 18:48:18 +0100

    separate the package list from the install script

---
+++ install-apps-common.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# script to install the server tools we commonly need
+
+set -e
+
+# if dnf is available, then use dnf, otherwise use yum, as the package manager command
+PM=yum
+if test -f /usr/bin/dnf
+then
+   PM=dnf
+fi
+
+cat packages-list-common | xargs $PM install -y
+
+echo "Finished successfully"
