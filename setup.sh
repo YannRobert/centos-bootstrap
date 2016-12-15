2fe3c3c0a662aa3ef8ba438f070621ddefcb7418
Yann ROBERT <yann.robert@anantaplex.fr>
2016-12-15 14:59:34 +0100

    fixed missing gpgkey file

--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ DISTRIB_NAME=$(awk '{print $1}' /etc/redhat-release)
 # copy repo files to yum.repos.d directory
 # repo files are embedded in the git repository for easier offline usage
 cp repos-$DISTRIB_NAME/*.repo /etc/yum.repos.d
+cp repos-$DISTRIB_NAME/RPM-GPG* /etc/yum.repos.d
 
 pushd ./downloaders
 for LOCAL_SCRIPT in $(ls *.sh)
