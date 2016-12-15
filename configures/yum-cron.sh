b1b6ee5bda42b1a5f863843a4ee399ec45e24cac
Yann ROBERT <yann.robert@anantaplex.fr>
2016-12-15 15:17:44 +0100

    only configure yum-cron if yum-cron.conf exists

--- configures/yum-cron.sh
+++ configures/yum-cron.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
-perl -i -pe 's/^apply_updates = no/apply_updates = yes/' /etc/yum/yum-cron.conf
-perl -i -pe 's/^update_cmd = default/update_cmd = minimal-security/' /etc/yum/yum-cron.conf
-chkconfig yum-cron on
-systemctl restart yum-cron.service
+if test -f /etc/yum/yum-cron.conf
+then
+  perl -i -pe 's/^apply_updates = no/apply_updates = yes/' /etc/yum/yum-cron.conf
+  perl -i -pe 's/^update_cmd = default/update_cmd = minimal-security/' /etc/yum/yum-cron.conf
+  chkconfig yum-cron on
+  systemctl restart yum-cron.service
+fi
