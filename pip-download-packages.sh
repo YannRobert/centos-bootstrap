a392d85836ae56565dd1ef72830a7c1b356e1c27
Yann ROBERT <yann.robert@anantaplex.fr>
2016-12-15 17:50:34 +0100

    don't change directory for pip

--- pip-download-packages.sh
+++ pip-download-packages.sh
@@ -3,6 +3,6 @@ set -e
 set -x
 
 mkdir -p ./downloaded_resources/pip-packages
-cd ./downloaded_resources/pip-packages
+#cd ./downloaded_resources/pip-packages
 
 pip download --dest ./downloaded_resources/pip-packages -r ./pip-packages-requirements.txt
