416d89aef69caa8736e5ef9002bf9ce9ce89d7f6
Yann ROBERT <yann.robert@anantaplex.fr>
2017-01-05 11:41:53 +0100

    fix for offline installation

--- pip-download-packages.sh
+++ pip-download-packages.sh
@@ -5,4 +5,9 @@ set -x
 mkdir -p ./downloaded_resources/pip-packages
 #cd ./downloaded_resources/pip-packages
 
+# shyaml requires d2to1
+# for some reason, it must be installed in a previous separated command
+# so let's download it also in a separated command
+pip download --dest ./downloaded_resources/pip-packages d2to1
+
 pip download --dest ./downloaded_resources/pip-packages -r ./pip-packages-requirements.txt
--- pip-install-packages.sh
+++ pip-install-packages.sh
@@ -5,4 +5,8 @@ set -x
 mkdir -p ./downloaded_resources/pip-packages
 #cd ./downloaded_resources/pip-packages
 
+# shyaml requires d2to1
+# for some reason, it must be installed in a previous separated command
+pip install --no-index --find-links ./downloaded_resources/pip-packages --upgrade d2to1
+
 pip install --no-index --find-links ./downloaded_resources/pip-packages --upgrade -r ./pip-packages-requirements.txt
